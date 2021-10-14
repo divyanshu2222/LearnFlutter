9f2ded43751b921004ff7529a7103f6f19cdd94b
Divyanshu Gupta <divyanshubuddy.2000@gmail.com>
2021-10-14 12:53:52 +0530

    Day 26 Completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -60,14 +60,7 @@ class _CartTotal extends StatelessWidget {
   }
 }
 
-class _CartList extends StatefulWidget {
-  const _CartList({Key? key}) : super(key: key);
-
-  @override
-  __CartListState createState() => __CartListState();
-}
-
-class __CartListState extends State<_CartList> {
+class _CartList extends StatelessWidget {
   final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
@@ -80,7 +73,6 @@ class __CartListState extends State<_CartList> {
                   trailing: IconButton(
                       onPressed: () {
                         _cart.remove(_cart.items[index]);
-                        setState(() {});
                       },
                       icon: Icon(Icons.remove_circle_outline)),
                   title: _cart.items[index].name.text.make(),

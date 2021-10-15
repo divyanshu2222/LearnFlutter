dca99854f40a25ff3cebb5b0bd2cbd849f5a0a11
Divyanshu Gupta <divyanshubuddy.2000@gmail.com>
2021-10-15 11:30:10 +0530

    Day 29 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_catalog/core/store.dart';
+import 'package:flutter_catalog/models/cart.dart';
 import 'dart:convert'; // It gives JSON decoder incoder
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/utils/routes.dart';
@@ -8,6 +9,7 @@ import 'package:flutter_catalog/widgets/home_widgets/catalog_header.dart';
 import 'package:flutter_catalog/widgets/home_widgets/catalog_list.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -18,6 +20,7 @@ class _HomePageState extends State<HomePage> {
   final int days = 30;
 
   final String name = "Divyanshu Gupta";
+  final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
 
 // It gives data before calling build method
   @override
@@ -28,8 +31,11 @@ class _HomePageState extends State<HomePage> {
 
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
-    final catalogJson =
-        await rootBundle.loadString("assets/files/catalog.json");
+    // final catalogJson =
+    //     await rootBundle.loadString("assets/files/catalog.json");
+    final response =
+        await http.get(Uri.parse(url));
+    final catalogJson = response.body;
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
     CatalogModel.items = List.from(productsData)
@@ -40,13 +46,20 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     // final dummyList = List.generate(5, (index) => CatalogModel.items[0]);
     return Scaffold(
         backgroundColor: Mytheme.creamColor,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-          backgroundColor: Mytheme.darkBluishColor,
-          child: Icon(CupertinoIcons.cart),
+        floatingActionButton: VxBuilder(
+          mutations: (AddMutation, RemoveMutation),
+          builder: (ctx, _) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            backgroundColor: Mytheme.darkBluishColor,
+            child: Icon(
+              CupertinoIcons.cart,
+              color: Colors.white,
+            ),
+          ).badge(color: Vx.red500, size: 22, count: _cart.items.length,textStyle: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,)),
         ),
         body: SafeArea(
           child: Container(
